ffb93c68411057a8571746b0160e16a8f1f4cf8c
Pedro <pedro@zoroboak.com>
2020-05-21 01:01:18 +0200

    Subida inicial del proyecto
    Subida inicial del código de la aplicación

---
+++ android/app/src/main/kotlin/com/example/peliculas_en_cines/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.peliculas_en_cines
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:peliculas_en_cines/src/vista/home_page.dart';
+import 'package:peliculas_en_cines/src/vista/pelicula_detalle.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Peliculas',
+      initialRoute: '/',
+      routes: { //Rutas de la aplicación, definimos los activitys entre los que navegará la aplicación
+        '/'       : ( BuildContext context ) => HomePage(),
+        'detalle' : ( BuildContext context ) => PeliculaDetalle(),
+      },
+    );
+  }
+}
